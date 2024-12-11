36bdeea3586b52b18139597d231460b64b9592b1
IT21105920 <it21105920@my.sliit.lk>
2024-12-11 21:15:11 +0530

    Changes for CountLetters

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -17,4 +17,66 @@ public class CountLettersTest {
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void firstWordDoesNotMatch(){
+        int words = new CountLetters().count("cat|dogs");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void NotMatchingWords(){
+        int words = new CountLetters().count("cat|dog");
+        Assertions.assertEquals(0, words);
+    }
+
+    @Test
+    public void OneMatchingWordEndWithR() {
+        int words = new CountLetters().count("moon|star");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void endsWithRfollowedByCharacter() {
+        int words = new CountLetters().count("car|");
+        Assertions.assertEquals(1, words);
+    }
+
+
+    @Test
+    public void OneLetterWord(){
+        int words = new CountLetters().count("s");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void onlyNonLetterCharacters() {
+        int words = new CountLetters().count("|!@#$");
+        Assertions.assertEquals(0, words);
+    }
+
+    @Test
+    public void endsWithNonLetterAfterMatch() {
+        int words = new CountLetters().count("stars!!");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void mixedCaseLetters() {
+        int words = new CountLetters().count("Stars|Rise");
+        Assertions.assertEquals(1, words);
+    }
+
+
+    @Test
+    public void trailingSpacesAfterMatchingWord() {
+        int words = new CountLetters().count("rocks ");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void NoWords(){
+        int words = new CountLetters().count("");
+        Assertions.assertEquals(0, words);
+    }
+
 }
